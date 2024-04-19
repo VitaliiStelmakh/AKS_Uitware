92cbb6d63dad99540453d33886a8028014b867a2
Perehinets Maksym <supercomf128@gmail.com>
2024-04-19 14:20:45 +0300

    canary deployment cheak

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while true; do
-    curl -ksS https://test.loc/api/v1/foods
+    curl -ksS https://bestrong.eastus.cloudapp.azure.com/api/v1/foods
     sleep 1  # Adjust the sleep duration as needed
 done
