78a21e6c23a5d8e35400d4be19c36d718c84e8b2
Perehinets Maksym <supercomf128@gmail.com>
2024-04-19 14:27:42 +0300

    test cananry 2

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while true; do
-    curl -ksS https://bestrong.eastus.cloudapp.azure.com/api/v1/foods
+    curl -ksS https://bestrong.eastus.cloudapp.azure.com/
     sleep 1  # Adjust the sleep duration as needed
 done
